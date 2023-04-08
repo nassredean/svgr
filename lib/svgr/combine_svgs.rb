e1387e568c20d970bc802966536ab5689455a020
Nassredean Nasseri <nassredean@gmail.com>
2023-04-08 00:04:26 -0400

    Feature: Center SVG elements within combined SVG document
    - Update the create_combined_svg method to properly center SVG elements within the combined SVG document
    - Adjust SVG document size to accommodate the contents and margins
    - Update the spec file to test the new centering behavior and margin options

--- lib/svgr/combine_svgs.rb
+++ lib/svgr/combine_svgs.rb
@@ -11,51 +11,47 @@ module Svgr
         sort: "default"
       }
 
-      opt_parser = OptionParser.new do |opts|
-        opts.banner =
-          "Usage: svgr combine [options] <source_directory> <rows> <columns>"
+      opt_parser =
+        OptionParser.new do |opts|
+          opts.banner =
+            "Usage: svgr combine [options] <source_directory> <rows> <columns>"
 
-        opts.on(
-          "-s",
-          "--scaling-factor FACTOR",
-          Float,
-          "Scaling factor for the SVG elements"
-        ) { |s| options[:scaling_factor] = s }
+          opts.on(
+            "-s",
+            "--scaling-factor FACTOR",
+            Float,
+            "Scaling factor for the SVG elements"
+          ) { |s| options[:scaling_factor] = s }
 
-        opts.on(
-          "-t",
-          "--margin-top MARGIN",
-          Integer,
-          "Top margin between the SVG elements"
-        ) { |t| options[:margin_top] = t }
+          opts.on(
+            "-t",
+            "--margin-top MARGIN",
+            Integer,
+            "Top margin between the SVG elements"
+          ) { |t| options[:margin_top] = t }
 
-        opts.on(
-          "-l",
-          "--margin-left MARGIN",
-          Integer,
-          "Left margin between the SVG elements"
-        ) { |l| options[:margin_left] = l }
+          opts.on(
+            "-l",
+            "--margin-left MARGIN",
+            Integer,
+            "Left margin between the SVG elements"
+          ) { |l| options[:margin_left] = l }
 
-        opts.on(
-          "--sort SORT",
-          %w[default random],
-          "Sorting option for the SVG files (default, random)"
-        ) { |sort| options[:sort] = sort }
+          opts.on(
+            "--sort SORT",
+            %w[default random],
+            "Sorting option for the SVG files (default, random)"
+          ) { |sort| options[:sort] = sort }
 
-        opts.on(
-          "--out FILE",
-          "Specify an output file path"
-        ) { |file| options[:out] = file }
+          opts.on("--out FILE", "Specify an output file path") do |file|
+            options[:out] = file
+          end
 
-        opts.on(
-          "-h",
-          "--help",
-          "Prints this help"
-        ) do
+          opts.on("-h", "--help", "Prints this help") do
             puts opts
             exit
           end
-      end
+        end
       opt_parser.parse!(argv)
 
       if argv.length < 3
@@ -66,21 +62,24 @@ module Svgr
       source_directory, rows, columns = argv.shift(3)
       rows = rows.to_i
       columns = columns.to_i
-      svg_files = list_svg_files(source_directory, options[:sort])[0...rows * columns]
+      svg_files =
+        list_svg_files(source_directory, options[:sort])[0...rows * columns]
 
-      combined_elements = svg_files.flat_map do |file|
-        content = read_svg_file(file)
-        extract_svg_elements(content)
-      end
+      combined_elements =
+        svg_files.flat_map do |file|
+          content = read_svg_file(file)
+          extract_svg_elements(content)
+        end
 
-      combined_svg = create_combined_svg(
-        combined_elements,
-        rows,
-        columns,
-        options[:scaling_factor],
-        options[:margin_top],
-        options[:margin_left]
-      )
+      combined_svg =
+        create_combined_svg(
+          combined_elements,
+          rows,
+          columns,
+          options[:scaling_factor],
+          options[:margin_top],
+          options[:margin_left]
+        )
 
       write_svg(combined_svg, options[:out])
     end
@@ -113,9 +112,7 @@ module Svgr
         svg_element = doc.at_xpath("//svg")
         new_group = Nokogiri::XML::Node.new("g", doc)
 
-        svg_element.children.each do |child|
-          new_group.add_child(child)
-        end
+        svg_element.children.each { |child| new_group.add_child(child) }
 
         svg_element.add_child(new_group)
         top_level_groups = [new_group]
@@ -132,16 +129,34 @@ module Svgr
       margin_top,
       margin_left
     )
+      width =
+        columns * 100 * scaling_factor +
+          (columns - 1) * margin_left * scaling_factor
+      height =
+        rows * 100 * scaling_factor + (rows - 1) * margin_top * scaling_factor
+
       combined_svg =
         Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
-          xml.svg(xmlns: "http://www.w3.org/2000/svg") do
+          xml.svg(
+            xmlns: "http://www.w3.org/2000/svg",
+            width: width,
+            height: height
+          ) do
             elements.each_with_index do |element, index|
               row = index / columns
               col = index % columns
 
               # Adjust the 'transform' attribute to position and scale the element in the grid
-              transform =
-                "translate(#{col * (100 + margin_left) * scaling_factor}, #{row * (100 + margin_top) * scaling_factor}) scale(#{scaling_factor})"
+              x =
+                col * (100 * scaling_factor + margin_left * scaling_factor) +
+                  (width - 100 * scaling_factor) / 2
+              y = row * (100 * scaling_factor + margin_top * scaling_factor)
+
+              # Offset by half the size of the element vertically and horizontally
+              x += 50 * scaling_factor
+              y += 50 * scaling_factor
+
+              transform = "translate(#{x}, #{y}) scale(#{scaling_factor})"
               element["transform"] = transform
               xml << element.to_xml
             end
@@ -149,7 +164,6 @@ module Svgr
         end
       combined_svg.to_xml
     end
-
 
     def self.write_svg(svg, output_file = nil)
       if output_file
--- spec/combine_svgs_spec.rb
+++ spec/combine_svgs_spec.rb
@@ -9,14 +9,12 @@ RSpec.describe Svgr::CombineSvgs do
     let(:columns) { 3 }
     let(:argv) { [fixtures_path, rows, columns] }
 
-    let(:output) do
-      capture_stdout { Svgr::CombineSvgs.start(argv) }
-    end
+    let(:output) { capture_stdout { Svgr::CombineSvgs.start(argv) } }
 
     it "combines the specified number of SVGs" do
-      doc = Nokogiri::XML(output)
+      doc = Nokogiri.XML(output)
       doc.remove_namespaces!
-      combined_elements = doc.xpath("//svg/g") 
+      combined_elements = doc.xpath("//svg/g")
       expect(combined_elements.size).to eq(rows * columns)
     end
 
@@ -24,11 +22,19 @@ RSpec.describe Svgr::CombineSvgs do
       let(:margin_top) { 10 }
       let(:margin_left) { 20 }
       let(:argv) do
-        [fixtures_path, rows.to_s, columns.to_s, "--margin-top", margin_top.to_s, "--margin-left", margin_left.to_s]
+        [
+          fixtures_path,
+          rows.to_s,
+          columns.to_s,
+          "--margin-top",
+          margin_top.to_s,
+          "--margin-left",
+          margin_left.to_s
+        ]
       end
 
       it "applies the specified margin options to the combined SVG" do
-        doc = Nokogiri::XML(output)
+        doc = Nokogiri.XML(output)
         doc.remove_namespaces!
         combined_elements = doc.xpath("//svg/g")
 
@@ -36,8 +42,12 @@ RSpec.describe Svgr::CombineSvgs do
           row = index / columns
           col = index % columns
 
+          x =
+            col * (100 + margin_left) + (doc.root["width"].to_f - 100) / 2 + 50
+          y = row * (100 + margin_top) + 50
+
           transform = element["transform"]
-          expected_transform = "translate(#{col * (100 + margin_left)}, #{row * (100 + margin_top)})"
+          expected_transform = "translate(#{x.to_i}, #{y.to_i})"
 
           expect(transform).to include(expected_transform)
         end
@@ -46,14 +56,14 @@ RSpec.describe Svgr::CombineSvgs do
 
     context "when sort option is specified" do
       let(:sort_option) { "random" }
-      let(:argv) { [fixtures_path, rows.to_s, columns.to_s, "--sort", sort_option] }
+      let(:argv) do
+        [fixtures_path, rows.to_s, columns.to_s, "--sort", sort_option]
+      end
 
       it "sorts the SVGs using the specified option" do
         # Run the command multiple times to check for randomness
         outputs = []
-        3.times do
-          outputs << capture_stdout { Svgr::CombineSvgs.start(argv) }
-        end
+        3.times { outputs << capture_stdout { Svgr::CombineSvgs.start(argv) } }
 
         # Check if the outputs are different, indicating randomness
         unique_outputs = outputs.uniq
