0d1b73b3e14ebf6db945431e77b513cbef566869
Nassredean Nasseri <nassredean@gmail.com>
2023-04-16 17:05:18 -0400

    update script

--- scripts/process_svgs.fish
+++ scripts/process_svgs.fish
@@ -24,8 +24,15 @@ while test $group_counter -le $num_groups
     set trimmed_files (string trim -r -c "," $joined_files)
     echo $trimmed_files
 
-    set output_file (string join "" $output_directory $group_counter ".svg")
-    eval "./bin/svgr arrange:grid $trimmed_files 3 1 -s 3 -t 8 | ./bin/svgr document:resize 228.6 279.4 | ./bin/svgr document:recolor \"#000000\" > $output_file"
+    set root_names
+    for file in $batch_files
+        set file_basename (basename $file .svg)
+        set root_names $root_names $file_basename
+    end
+    set output_file_name (string join "__" $root_names)
+    set output_file (string join "" $output_directory $output_file_name ".svg")
+
+    eval "./bin/svgr arrange:grid $trimmed_files 3 1 -s 3 -t 8 | ./bin/svgr document:resize 228.6 304.8 | ./bin/svgr document:recolor \"#000000\" > $output_file"
 
     set group_counter (math "$group_counter + 1")
 end
